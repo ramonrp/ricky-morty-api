50682927e65d8ff63bd1c9c80a408209e5a595bf
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-01-17 12:53:50 +0100

    feat: create and use router component

---
+++ src/router/router.component.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { switchRoutes } from './routes';
+import { CharacterList, CharacterDetail, About } from '../scenes';
+import { AppLayout } from '../layouts';
+const AppRouter: React.FC = () => {
+  return (
+    <Router>
+      <AppLayout>
+        <Routes>
+          <Route path={switchRoutes.characterList} element={<CharacterList />}></Route>
+          <Route path={switchRoutes.about} element={<About />}></Route>
+          <Route path={switchRoutes.characterDetail} element={<CharacterDetail />}></Route>
+        </Routes>
+      </AppLayout>
+    </Router>
+  );
+};
+
+export { AppRouter };
