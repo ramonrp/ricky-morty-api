94713def26660c7fd1157a60935d71864990fbb3
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-01-17 16:44:31 +0100

    feat: get Characters and navigate to detail

--- src/pods/character-list/character-list.container.tsx
+++ src/pods/character-list/character-list.container.tsx
@@ -1,20 +1,31 @@
+import { Link } from 'react-router-dom';
 import useSWR from 'swr';
 import { getCharacters } from './api/api';
 import { CharacterListFromApiToVm } from './character-list.mapper';
 import { Character } from './character-list.vm';
 
 const URL = 'https://rickandmortyapi.com/api/character';
-
+const fetcher = async (url: string): Promise<Character[]> => {
+  const result = await getCharacters(url);
+  const charactersApi = result.results;
+  const characterVM = CharacterListFromApiToVm(charactersApi);
+  return characterVM;
+};
 const CharacterListContainer: React.FC = () => {
-  const fetcher = async (url: string): Promise<Character[]> => {
-    const result = await getCharacters(url);
-    const charactersApi = result.results;
-    const characterVM = CharacterListFromApiToVm(charactersApi);
-    return characterVM;
-  };
-  const { data, error } = useSWR(URL, fetcher);
-  console.log(data);
-  return <ul></ul>;
+  const { data: characters, error } = useSWR(URL, fetcher);
+  if (error) return <h1>There was an error fetching characters</h1>;
+  if (!characters) {
+    return <h1>Loading...</h1>;
+  }
+  return (
+    <ul>
+      {characters.map(character => (
+        <Link to={`/${character.id}`} key={character.id}>
+          <li>{character.name}</li>
+        </Link>
+      ))}
+    </ul>
+  );
 };
 
 export { CharacterListContainer };
