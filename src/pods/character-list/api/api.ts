74b49b81f983ee3871b21c1c5c74a0dc6f154e89
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-01-18 14:34:13 +0100

    typo species query

--- src/pods/character-list/api/api.ts
+++ src/pods/character-list/api/api.ts
@@ -15,7 +15,7 @@ export function getCharacters(
 ): Promise<Response> {
   const queryName = name ? `&name=${name}` : '';
   const queryGender = gender ? `&gender=${gender}` : '';
-  const querySpecie = specie ? `&specie=${specie}` : '';
+  const querySpecie = specie ? `&species=${specie}` : '';
   const queryStatus = status ? `&status=${status}` : '';
   const endpointQuery = url + '?' + queryName + queryGender + querySpecie + queryStatus;
   const hasQueries = queryName || queryGender || querySpecie || queryStatus;
