c789ee908d8838cc0fd494ce72f9ce0f959c3b02
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-01-20 19:21:08 +0100

    fix: url variable typo

--- src/pods/character-detail/hooks/useCharacterToVm.ts
+++ src/pods/character-detail/hooks/useCharacterToVm.ts
@@ -2,7 +2,7 @@ import useSWR from 'swr';
 import { Character } from '../character-detail.vm';
 import { characterFromApiToVM } from '../character.detail.mapper';
 import { getCharacterById } from '../api';
-const URL = process.env.BASE_RICKY_API;
+const URL = 'https://rickandmortyapi.com/api/character/';
 const useCharacterToVM = (id: string) => {
   const result = useSWR([URL, id], getCharacterById);
   return {
--- src/pods/character-list/hooks/useCharacterListToVM.ts
+++ src/pods/character-list/hooks/useCharacterListToVM.ts
@@ -2,7 +2,7 @@ import useSWR from 'swr';
 import { getCharacters } from '../api';
 import { CharacterListFromApiToVm } from '../character-list.mapper';
 
-const URL = process.env.BASE_RICKY_API;
+const URL = 'https://rickandmortyapi.com/api/character/';
 
 const useCharacterListToVM = (
   name: string,
